aed90bfdfbaa4a5342aaa2770fcbb64eab2a634c
DESKTOP-SBLFB76\1 <richtrr@ya.ru>
2026-01-29 13:40:36 +0300

    Fixed title

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,6 +2,6 @@ import { define } from "../utils.ts";
 import Form from "../islands/Form.tsx";
 
 export default define.page(function Home(ctx) {
-  ctx.state.title = "";
+  ctx.state.title = "Green-API test Fresh";
   return <Form />;
 });
