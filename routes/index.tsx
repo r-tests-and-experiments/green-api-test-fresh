845b8d4f57d5f251b956b3dc315646549fe45868
DESKTOP-SBLFB76\1 <richtrr@ya.ru>
2026-01-29 11:45:15 +0300

    Fixed

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,7 @@
 import { define } from "../utils.ts";
 import Form from "../islands/Form.tsx";
 
-export default define.page(() => {
-  return (
-    <div>
-      <Form />
-    </div>
-  );
+export default define.page(function Home(ctx) {
+    ctx.state.title = "Green-API test Fresh";
+    return (<Form />);
 });
